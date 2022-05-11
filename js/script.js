443cd50383d6376e3ba958ecc6cad05665f0fb66
Egor Dubovik <egorka54896@gmail.com>
2022-05-11 22:29:24 +0300

    feat: add base data

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const API_KIY = "107281430eadbfeb34098c3fdc1d890c";
+const BASE_URL = "http://api.openweathermap.org/data/2.5/forecast?";
+const LANGUAGE = "language=ru-RU";
+//`http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}
+//&limit={limit}&units=metric&appid={API key}`
+
+//units=imperial - Fahrenheit / Celsius use units=metric
+
