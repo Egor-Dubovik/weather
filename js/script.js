0beba0c5eb5d535a0567dd1283648cf3e64759c0
Egor Dubovik <egorka54896@gmail.com>
2022-05-16 11:51:44 +0300

    feat: created function 'initMap' (js)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import { Weather } from "./modules/weather.js";
 import { showCurrentDate } from "./modules/showCurrentDate.js";
+// import { initMap } from "./modules/initMap.js";
+import { getDate } from "./modules/getDate.js";
 
 
 const weather = new Weather;
@@ -10,4 +12,30 @@ console.log(weather.showForecast());
 
 
 
+let center = [59.94, 30.32];
+// initMap(center);
 
+const initMap = () => {
+	const map = new ymaps.Map("map", {
+		center: center,
+		zoom: 16,
+	});
+
+	const placemark = new ymaps.Placemark(center,
+		{
+			balloonContentHeader: 'Хедер балуна',
+		},
+		{
+			iconLayout: 'default#image',
+			iconImageHref: '../assets/img/marker.png',
+			iconImageSize: [40, 40],
+			iconImageOffset: [-20, -40]
+		}
+	);
+
+	map.controls.add('zoomControl', { top: 10, left: 5 });
+	map.geoObjects.add(placemark);
+}
+
+
+ymaps.ready(initMap);
